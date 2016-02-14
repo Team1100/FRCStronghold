f3eb5010dfde460a8b73a52bdc7501d0db4ec613
Zach the Magnamonous <zacharysamiyoussef@gmail.com>
2016-02-14 17:38:48 -0500

    Cleaned up ultrasound code- now callable/functional

--- src/org/usfirst/frc/team1100/robot/subsystems/Ultrasound.java
+++ src/org/usfirst/frc/team1100/robot/subsystems/Ultrasound.java
@@ -9,90 +9,83 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Ultrasound extends Subsystem{
+public class Ultrasound extends Subsystem {
 
-	AnalogInput EZ1;
-	AnalogInput EZ3;
-	
+	private AnalogInput EZ1;
+	private AnalogInput EZ3;
+	private final int SAMPLE_SIZE = 100;
 	private Ultrasound ultrasound;
-	
-	public Ultrasound getInstance(){
-		if(ultrasound==null)ultrasound = new Ultrasound();
+
+	public Ultrasound getInstance() {
+		if (ultrasound == null)
+			ultrasound = new Ultrasound();
 		return ultrasound;
 	}
-	
-	public Ultrasound(){
+
+	public Ultrasound() {
 		EZ1 = new AnalogInput(RobotMap.U_EZ1);
 		EZ3 = new AnalogInput(RobotMap.U_EZ3);
 	}
-/*protected void execute() {
+
+	public double getDistanceEZ3() {
+		ArrayList<Double> data = new ArrayList<Double>();
 		
-		if(System.currentTimeMillis() - oldTime >= refreshRate) {
-			data.add((double) Drive.getInstance().getUltrasound());
-			oldTime = System.currentTimeMillis();
-			if(data.size()==sampleSize){
-				Collections.sort(data);
-				double value = getMode(data);
-				SmartDashboard.putNumber("Ultrasonic Input", value);
-				SmartDashboard.putNumber("Distance (inches)", distanceMZ3(value));
-				data.clear();
-			}
+		for(int i = 0; i < SAMPLE_SIZE; i++){
+			data.add((double) (EZ3.getValue()));
 		}
-	}
-
-	private double distanceMZ3(double value){
-		return .129*value + 2.494; 
-	}
-	
-	private double distanceMZ1(double value){
-		return .129*value + 2.077;
+		Collections.sort(data);
+		double value = getMode(data);
+		data.clear();
+		return distanceEZ3(value);
 	}
 	
-	@Override
-	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return false;
+	public double getDistanceEZ1() {
+		ArrayList<Double> data = new ArrayList<Double>();
+		
+		for(int i = 0; i < SAMPLE_SIZE; i++){
+			data.add((double) (EZ1.getValue()));
+		}
+		Collections.sort(data);
+		double value = getMode(data);
+		data.clear();
+		return distanceEZ1(value);
 	}
 
-	@Override
-	protected void end() {
-		// TODO Auto-generated method stub
-		
+	private double distanceEZ3(double value) {
+		return .129 * value + 2.494;
 	}
 
-	@Override
-	protected void interrupted() {
-		// TODO Auto-generated method stub
-		
+	private double distanceEZ1(double value) {
+		return .129 * value + 2.077;
 	}
-	public double getMode(ArrayList<Double> list){
+
+	public double getMode(ArrayList<Double> list) {
 		double x = 0;
 		double y = 0;
 		double count = 0;
 		double maxCount = 0;
 		int index = 0;
-		for(int i = 0; i< list.size(); i++){
+		for (int i = 0; i < list.size(); i++) {
 			x = list.get(i);
-			if(x==y){
+			if (x == y) {
 				count++;
-			}
-			else{
-				if(count>maxCount){
-					maxCount=count;
-					index = i-1;
+			} else {
+				if (count > maxCount) {
+					maxCount = count;
+					index = i - 1;
 				}
 				count = 0;
-				y=x;
+				y = x;
 			}
 		}
 		return list.get(index);
-		
-	}*/
+
+	}
 
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
